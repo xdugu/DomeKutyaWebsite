c320e811656891e9a0cef980612b27d2662eeecc
xdugu <xdugu2007@gmail.com>
2019-04-23 21:44:07 +0200

    Fixed Basket
    Need to fix product page when item does not have  variant
    
    Also in the basket.html, need to restore "item is varaint" property

--- scripts/basket.js
+++ scripts/basket.js
@@ -12,18 +12,20 @@ Storage.prototype.getObj = function(key) {
 var app = angular.module('myApp',[]);
 app.controller('Basket', function($scope, $http) {
 	$scope.order=[];//localStorage.getObj("order");
-	$scope.currency = "EUR"
+	$scope.currency = "HUF"
 	$scope.basketId = localStorage.getObj("basketId");
+	$scope.costs={};
+	$scope.contact={country:"Hungary"};
 	 $http({
 				method: 'POST',
 				crossDomain : true,
 				url: 'https://0j7ds3u9r6.execute-api.eu-central-1.amazonaws.com/v2/Request/Basket/GetBasket',
-				data: JSON.stringify({basketId:$scope.basketId}),
+				data: JSON.stringify({basketId:$scope.basketId, includeCost: true, country:$scope.contact.country}),
 				headers: {'Content-Type': 'application/json'}
 			}).then(function(res){
 				if(res.data.Result=="OK"){
-					let temp = JSON.parse(res.data.data);
-					$scope.order = temp.Item.Items;
+					let temp = res.data.data;
+					$scope.order = temp.Item;
 					$(".basket-num").html( temp.Item.Items.length);
 				}
 			});
@@ -31,12 +33,12 @@ app.controller('Basket', function($scope, $http) {
 
 	
 	$scope.changeQuantity= function (index, direction){
-		if($scope.order[index].Quantity + direction>=1){
+		if($scope.order.Items[index].Quantity + direction>=1){
 		$http({
 				method: 'POST',
 				crossDomain : true,
 				url: 'https://0j7ds3u9r6.execute-api.eu-central-1.amazonaws.com/v2/Request/Basket/ChangeQuantity',
-				data: JSON.stringify({basketId:$scope.basketId, index: index, increment: direction}),
+				data: JSON.stringify({basketId:$scope.basketId, index: index, increment: direction, country:$scope.contact.country}),
 				headers: {'Content-Type': 'application/json'}
 			}).then(function(res){
 				if(res.data.Result=="OK"){
@@ -52,7 +54,7 @@ app.controller('Basket', function($scope, $http) {
 				method: 'POST',
 				crossDomain : true,
 				url: 'https://0j7ds3u9r6.execute-api.eu-central-1.amazonaws.com/v2/Request/Basket/RemoveItem',
-				data: JSON.stringify({basketId:$scope.basketId, index: index}),
+				data: JSON.stringify({basketId:$scope.basketId, index: index, country:$scope.contact.country}),
 				headers: {'Content-Type': 'application/json'}
 			}).then(function(res){
 				if(res.data.Result=="OK"){
@@ -64,7 +66,19 @@ app.controller('Basket', function($scope, $http) {
 	
 	$scope.updateDeliveryCost = function()
 	{
-		$scope.order=Shop_updateContact($scope.order.contact);
+		$http({
+				method: 'POST',
+				crossDomain : true,
+				url: 'https://0j7ds3u9r6.execute-api.eu-central-1.amazonaws.com/v2/Request/Basket/GetBasket',
+				data: JSON.stringify({basketId:$scope.basketId, includeCost: true, country:$scope.contact.country}),
+				headers: {'Content-Type': 'application/json'}
+			}).then(function(res){
+				if(res.data.Result=="OK"){
+					let temp = res.data.data;
+					$scope.order = temp.Item;
+					$(".basket-num").html( temp.Item.Items.length);
+				}
+			});
 	}
 	
 	
